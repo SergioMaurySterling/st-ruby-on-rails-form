faec85d8933f14f18f70a21ec14281e9757d35bd
SergioMaurySterling <52435269+SergioMaurySterling@users.noreply.github.com>
2022-01-31 08:11:27 -0500

    new reports controllers and concerns

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,14 @@
+class ReportsController < ApplicationController
+  def hello
+    @credentials = Rails.application.credentials.hello
+    @variable = Pet.first.name
+  end
+  def customer
+    @credentials = Rails.application.credentials.hello
+    @variable = Pet.first.name
+  end
+  def service
+    @credentials = Rails.application.credentials.hello
+    @variable = Pet.first.name
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'hola', to: 'welcome#hello'
+  get 'hola', to: 'reports#hello'
+  get 'customer', to: 'reports#customer'
+  get 'service', to: 'reports#service'
 end
